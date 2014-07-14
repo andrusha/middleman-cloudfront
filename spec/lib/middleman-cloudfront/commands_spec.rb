9645706c40827b4de01300f61d57e72ff55265da
Manuel Meurer <manuel@krautcomputing.com>
2014-07-14 12:00:36 +0200

    Spec fixes for Rspec 3

--- spec/lib/middleman-cloudfront/commands_spec.rb
+++ spec/lib/middleman-cloudfront/commands_spec.rb
@@ -17,24 +17,22 @@ describe Middleman::Cli::CloudFront do
 
   describe '#invalidate' do
     before do
-      Fog::CDN::AWS::Distributions.any_instance.stub(:get).and_return(distribution)
-      distribution.invalidations.stub(:create) do
-        double('invalidation', status: 'InProgress', wait_for: ->{} )
+      allow_any_instance_of(Fog::CDN::AWS::Distributions).to receive(:get).and_return(distribution)
+      allow(distribution.invalidations).to receive(:create) do
+        double('invalidation', status: 'InProgress', wait_for: -> {})
       end
     end
 
     it 'gets the correct distribution' do
-      cloudfront.stub(:list_files).and_return []
+      allow(cloudfront).to receive(:list_files).and_return([])
       expect_any_instance_of(Fog::CDN::AWS::Distributions).to receive(:get).with('distribution_id_123')
       cloudfront.invalidate(options)
     end
 
     context 'when the amount of files to invalidate is under the limit' do
       it 'divides them up in packages and creates one invalidation per package' do
-        files = (1..Middleman::Cli::CloudFront::INVALIDATION_LIMIT).map do |i|
-          "file_#{i}"
-        end
-        cloudfront.stub(:list_files).and_return files
+        files = (1..Middleman::Cli::CloudFront::INVALIDATION_LIMIT).map { |i| "file_#{i}" }
+        allow(cloudfront).to receive(:list_files).and_return(files)
         expect(distribution.invalidations).to receive(:create).once.with(paths: files)
         cloudfront.invalidate(options)
       end
@@ -42,10 +40,8 @@ describe Middleman::Cli::CloudFront do
 
     context 'when the amount of files to invalidate is over the limit' do
       it 'creates only one invalidation with all of them' do
-        files = (1..(Middleman::Cli::CloudFront::INVALIDATION_LIMIT * 3)).map do |i|
-          "file_#{i}"
-        end
-        cloudfront.stub(:list_files).and_return files
+        files = (1..(Middleman::Cli::CloudFront::INVALIDATION_LIMIT * 3)).map { |i| "file_#{i}" }
+        allow(cloudfront).to receive(:list_files).and_return(files)
         expect(distribution.invalidations).to receive(:create).once.with(paths: files[0, Middleman::Cli::CloudFront::INVALIDATION_LIMIT])
         expect(distribution.invalidations).to receive(:create).once.with(paths: files[Middleman::Cli::CloudFront::INVALIDATION_LIMIT, Middleman::Cli::CloudFront::INVALIDATION_LIMIT])
         expect(distribution.invalidations).to receive(:create).once.with(paths: files[Middleman::Cli::CloudFront::INVALIDATION_LIMIT * 2, Middleman::Cli::CloudFront::INVALIDATION_LIMIT])
